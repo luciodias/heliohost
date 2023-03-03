095b6b1ad5e987420392786d7e6aaaa4122e2737
luciodias <luciodiass@gmail.com>
2023-03-03 14:23:24 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http'),url = require('url');
+
+const hostname = 'luciod.helioho.st';
+const port = 80;
+
+const server = http.createServer((req, res) => {
+	const query = url.parse(req.url,true).query;
+	res.statusCode = 200;
+  	res.setHeader('Content-Type', 'text/plain');
+	//const resJson = JSON.stringify(query); 
+	//res.end(query["hub.challenge"]);
+	res.end(process.env.var);
+});
+
+server.listen(port, hostname, () => {
+  //console.log(`Server running at http://${hostname}:${port}/`);
+});
